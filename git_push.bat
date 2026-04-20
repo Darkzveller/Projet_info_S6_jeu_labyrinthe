c1229555b03e9d6a832461d46911fdfddb63aad2
Darkzveller <elkattoufi.youssef@gmail.com>
2026-04-20 14:08:33 +0200

    generation fichier make

--- git_push.bat
+++ git_push.bat
@@ -1,2 +1,2 @@
 echo off
-git status && git add . && git commit -m "creation d'une variable contenant les données du labyrinth" && git push
+git status && git add . && git commit -m "generation fichier make" && git push
